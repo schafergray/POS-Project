69b532e22f3f3ff22cd22a7855a0b676540d909a
Schafer Gray <schafergray@Schafers-MacBook-Pro.local>
2024-02-15 21:48:21 -0500

    Small fix.

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 
-describe('PosComponent', () => {
+describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
